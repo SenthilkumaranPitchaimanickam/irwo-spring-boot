9463afc397a796d18f31281f4a12cf12fe65ad63
SenthilkumaranPitchaimanickam <83630199+SenthilkumaranPitchaimanickam@users.noreply.github.com>
2021-05-04 14:14:13 +0530

    Add files via upload

---
+++ src/main/java/com/albertsons/irwo/IrwoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.albertsons.irwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrwoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrwoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albertsons/irwo/IrwoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertsons.irwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrwoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
